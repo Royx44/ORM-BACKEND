794315e32150610450be8c5a4edad606fbf4012c
git@github.com:Royx44/prework-study-guide.git <rh863684@gmail.com>
2023-10-12 22:49:40 -0500

    edit code in category-routes.js

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -1,95 +1,66 @@
-const router = require('express').Router();
-const { Category, Product, ProductTag } = require('../../models');
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
 
-router.get('/', async (req, res) => {
-  try {
-    const categoryData = await Category.findAll({
-      include: { model: Product, through: ProductTag },
-    });
-
-    res.status(200).json(categoryData);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: 'An error occurred while fetching categories.' });
-  }
-});
-
-router.get('/:id', async (req, res) => {
-  try {
-    const categoryData = await Product.findOne({
-      where: { id: req.params.id },
-      include: [
-        Product,
-        {
-          model: Category,
-        },
-      ],
-    });
-
-    if (!categoryData) {
-      res.status(404).json({ message: 'Category not found with that ID.' });
-      return;
-    }
-
-    res.status(200).json(categoryData);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: 'An error occurred while fetching the category.' });
-  }
-});
-
-router.post('/', async (req, res) => {
-  try {
-    const categoryData = await Category.create({
-      category_name: req.body.category_name,
-    });
-
-    res.status(201).json(categoryData);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: 'An error occurred while creating the category.' });
-  }
-});
-
-router.put('/:id', async (req, res) => {
-  try {
-    const [updatedRows] = await Category.update(req.body, {
-      where: {
-        id: req.params.id,
+router.get("/", async (req, res) => {
+  // find all categories
+  // be sure to include its associated Products
+  const categories = await Category.findAll({
+    include: [
+      {
+        model: Product,
       },
-    });
+    ],
+  });
+  res.send(categories);
+});
 
-    if (updatedRows === 0) {
-      res.status(404).json({ message: 'Category not found with that ID.' });
-      return;
-    }
+router.get("/:id", async (req, res) => {
+  // find one category by its `id` value
+  // be sure to include its associated Products
+  const categoryId = req.params.id;
+  const category = await Category.findByPk(categoryId, {
+    include: [
+      {
+        model: Product,
+      },
+    ],
+  });
+  res.send(category);
+});
 
-    res.status(200).json({ message: 'Category updated successfully.' });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: 'An error occurred while updating the category.' });
+router.post("/", async (req, res) => {
+  // create a new category
+  const category = await Category.create(req.body);
+  res.send(category);
+});
+
+router.put("/:id", async (req, res) => {
+  // update a category by its `id` value
+  const categoryId = req.params.id;
+  const [count, data] = await Category.update(req.body, {
+    where: { id: categoryId },
+  });
+  if (count > 0) {
+    res.send("Category updated successfully!");
+  } else {
+    res.send("Nothing to be updated!");
   }
 });
 
-router.delete('/:id', async (req, res) => {
-  try {
-    const categoryData = await Category.destroy({
-      where: {
-        id: req.params.id,
-      },
-    });
-
-    if (!categoryData) {
-      res.status(404).json({ message: 'Category not found with that ID.' });
-      return;
-    }
-
-    res.status(200).json({ message: 'Category deleted successfully.' });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: 'An error occurred while deleting the category.' });
+router.delete("/:id", async (req, res) => {
+  // delete a category by its `id` value
+  const categoryId = req.params.id;
+  const category = await Category.destroy({
+    where: {
+      id: categoryId,
+    },
+  });
+  if (category > 0) {
+    res.send("Category deleted successfully!");
+  } else {
+    res.send("Nothing to  deleted!");
   }
 });
 
