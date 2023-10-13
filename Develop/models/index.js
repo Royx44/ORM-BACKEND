adc9cc68643d7827fb91d7af698fd67d150329a9
git@github.com:Royx44/prework-study-guide.git <rh863684@gmail.com>
2023-10-12 21:55:55 -0500

    deleted code in index.js in models folder

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -29,28 +29,3 @@ module.exports = {
   Tag,
   ProductTag,
 };
-
-// Rest of your code
-const express = require('express');
-const routes = require('./routes');
-const sequelize = require('./config/connection');
-
-const app = express();
-const PORT = process.env.PORT || 3001;
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(routes);
-
-const startServer = async () => {
-  try {
-    await sequelize.sync();
-    app.listen(PORT, () => {
-      console.log(`App is listening on port ${PORT}!`);
-    });
-  } catch (err) {
-    console.error('Error syncing sequelize:', err);
-  }
-};
-
-startServer();
